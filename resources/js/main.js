a0cf239de76f9cb0763ef249726c659ade5f1215
Carsten Höschele <hoeschele@digitalmanufaktur.com>
2022-05-05 11:45:21 +0200

    [gruppe3] added code from group 3

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,59 @@
-const db = document.querySelector("bla");
+/* variables */
+const apiKey = "05c3dc2ee4be01213995071eec5b9bcc";
+const baseUrl = "http://api.openweathermap.org/data/2.5/weather?";
+//const searchBtn = document.querySelector(".search-icon");
+const inputValue = document.querySelector(".input-field");
+let city = document.querySelector(".city");
+let temp = document.querySelector(".temp");
+let wetterlage = document.querySelector(".wetterlage");
+let fetched = false;
 
-function hello() {
-  console.log("hello");
+/* declare functions */
+function searchWeather() {
+  let citySearched = null;
+  let tempSearched = null;
+  let wetterlageSearched = null;
+
+  console.log('City: ' + inputValue.value)
+
+  fetch(
+    baseUrl +
+      "q=" +
+      inputValue.value +
+      "&appid=" +
+      apiKey +
+      "&units=metric&lang=de"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      citySearched = data.name;
+      tempSearched = Math.ceil(data.main.temp);
+      wetterlageSearched = data.weather[0].description;
+
+      changeValues(citySearched, tempSearched, wetterlageSearched);
+      inputValue.value = "";
+    });
 }
 
-db.addEventListener("click", hello);
+function searchWeatherEnter(event) {
+  if (event.key == "Enter") {
+    searchWeather();
+  }
+}
+
+function changeValues(citySearched, tempSearched, wetterlageSearched) {
+  city.innerText = citySearched;
+  temp.innerText = tempSearched + "°C";
+  wetterlage.innerText = wetterlageSearched;
+}
+
+if (!fetched) {
+  city.innerText = "--";
+  temp.innerText = "--" + "°C";
+  wetterlage.innerText = "Für Wetterlage eine Stadt eingeben..";
+}
+
+/* call functions */
+inputValue.addEventListener("keyup", searchWeatherEnter);
+//searchBtn.addEventListener("click", searchWeather);
